c2abde8710c103171b3dbfa84a94af9ab9891b97
BouvetEliott <eli.bvt61@hotmail.com>
2023-03-30 14:12:59 +0200

    check not empty inputs

--- js/defile.js
+++ js/defile.js
@@ -28,7 +28,30 @@ function previousSlide(){
     img.setAttribute('class','img-fluid');
 }
 
+
+
+
 window.onload = function(){
     document.getElementById('next').addEventListener('click',nextSlide);
     document.getElementById('previous').addEventListener('click',previousSlide);
+
+    // Example starter JavaScript for disabling form submissions if there are invalid fields
+    (() => {
+        'use strict';
+
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.querySelectorAll('.needs-validation');
+        console.log(forms);
+
+        // Loop over them and prevent submission
+        Array.prototype.slice.call(forms).forEach((form) => {
+        form.addEventListener('submit', (event) => {
+            if (!form.checkValidity()) {
+            event.preventDefault();
+            event.stopPropagation();
+            }
+            form.classList.add('was-validated');
+        }, false);
+        });
+    })();
 }
