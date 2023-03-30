a3ce63426c2d12b459826d3c991d2054734c0641
BouvetEliott <eli.bvt61@hotmail.com>
2023-03-30 13:54:31 +0200

    slider upgrade

--- js/defile.js
+++ js/defile.js
@@ -1,22 +1,34 @@
+var slides = ['img/draw2.svg','img/portfolio.jpeg','img/portfolio2.jpeg']; 
 
+var start=0;
 
-var slides = ['img/draw2.svg','img/portfolio.jpeg','img/draw2.svg','img/portfolio.jpeg','img/draw2.svg','img/portfolio.jpeg','img/draw2.svg',]; 
+setInterval(nextSlide,4000);
 
-var Start=0;
+function nextSlide(){   
+    var divImg = document.getElementById('divImg');
+    
+    start = (start+1)%slides.length;
 
-function slider(){
     var img = document.getElementById('img');
-    if(Start<slides.length){
-        Start=Start+1;
-    }
-    else{
-        Start=1;
-    }
-    console.log(img);
-    img.innerHTML = "<img src="+slides[Start-1]+">";
-   
+    img.setAttribute('src',slides[start]);
+    img.setAttribute('class','img-fluid');
 }
-setInterval(slider,2000);
 
+function previousSlide(){
+    var divImg = document.getElementById('divImg');
 
+    if(start == 0){
+        start = slides.length-1;
+    }else{
+        start--;
+    }
 
+    var img = document.getElementById('img');
+    img.setAttribute('src',slides[start]);
+    img.setAttribute('class','img-fluid');
+}
+
+window.onload = function(){
+    document.getElementById('next').addEventListener('click',nextSlide);
+    document.getElementById('previous').addEventListener('click',previousSlide);
+}
