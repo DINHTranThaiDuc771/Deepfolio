89471c8e58fe7ce2706b65a877f67ee1cd3a7f15
BouvetEliott <eli.bvt61@hotmail.com>
2023-03-30 16:37:06 +0200

    Update accueil.php

--- php/accueil.php
+++ php/accueil.php
@@ -3,6 +3,15 @@
 if (session_status() == PHP_SESSION_NONE) {
     //header('location: ./connexion.php');
     header('location: ../html/connexion.html');
+}else{
+
+    $DB = DB::getInstance();
+
+    $username = $_SESSION["username"];
+
+    $listePortfolios = $DB->getPortfolios($username);
+
+
 }
 
 
