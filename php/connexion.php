7f2a65fe9a725fd8848c1618e891cd308d880838
BouvetEliott <eli.bvt61@hotmail.com>
2023-03-31 13:25:26 +0200

    displayAlert (not working)

--- php/connexion.php
+++ php/connexion.php
@@ -1,57 +1,3 @@
-<?php
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-
-
-require '../server/DB.inc.php';
-// Initialize the session
-
-if (session_status() == PHP_SESSION_NONE) {
-  session_start();
-}
-
- 
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header('location: accueil.php');
-    exit;
-}
- 
- 
-// Define variables and initialize with empty values
-$username = $password = $status = "";
-$username_err = $password_err = $login_err = "";
- 
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
-    
-    $username = trim($_POST["username"]);   
-    $password = trim($_POST["password"]);
-    
-    $DB = DB::getInstance();
-
-    if($DB->userExists($username)){
-        $mdp = $DB->getMdp($username);
-
-        if (password_verify($password, $mdp)) {
-            $_SESSION["loggedin"] = true;
-            $_SESSION["username"] = $username;
-            header("location: accueil.php");
-        }else{
-            echo"<style> .alert {display: block;}</style>";
-        }
-    }else{
-        $login_err = "Nom inconnu";
-    }
-
-}
-
-?>
-
-
-
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -143,3 +89,55 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   </body>
 </html>
 
+
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+
+
+require '../server/DB.inc.php';
+// Initialize the session
+
+if (session_status() == PHP_SESSION_NONE) {
+  session_start();
+}
+
+ 
+// Check if the user is already logged in, if yes then redirect him to welcome page
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+    header('location: accueil.php');
+    exit;
+}
+ 
+ 
+// Define variables and initialize with empty values
+$username = $password = $status = "";
+$username_err = $password_err = $login_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+ 
+    
+    $username = trim($_POST["username"]);   
+    $password = trim($_POST["password"]);
+    
+    $DB = DB::getInstance();
+
+    if($DB->userExists($username)){
+        $mdp = $DB->getMdp($username);
+
+        if (password_verify($password, $mdp)) {
+            $_SESSION["loggedin"] = true;
+            $_SESSION["username"] = $username;
+            header("location: accueil.php");
+        }else{
+            //echo'<script> displayAlert() </script>';
+        }
+    }else{
+        $login_err = "Nom inconnu";
+    }
+
+}
+
+?>
