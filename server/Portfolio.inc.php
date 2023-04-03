f749c2915708bee2b37d8c39a2ec5897719bbfd9
Erreiip <lolpierre154@gmail.com>
2023-04-03 15:20:31 +0200

    Update Portfolio.inc.php

--- server/Portfolio.inc.php
+++ server/Portfolio.inc.php
@@ -2,21 +2,21 @@
 
     class Portfolio {
 
-        private $nomUtilisateur;
-        private $id;
-        private $nomPortfolio;
+        private $nomutilisateur;
+        private $idportfolio;
+        private $nomportfolio;
         private $accesible;
 
-        public function __construct( $nomUtilisateur = "", $id = 1, $nomPortfolio = "", $accesible = false ) {
-            $this->nomUtilisateur = $nomUtilisateur;
-            $this->id = $id;
-            $this->nomPortfolio = $nomPortfolio;
+        public function __construct( $nomutilisateur = "", $idportfolio = 1, $nomportfolio = "", $accesible = false ) {
+            $this->nomutilisateur = $nomutilisateur;
+            $this->idportfolio = $idportfolio;
+            $this->nomportfolio = $nomportfolio;
             $this->accesible = $accesible;
         }
 
-        public function getNomPortfolio() { return $this->nomPortfolio; }
-        public function getNomUtilisateur() { return $this->nomUtilisateur; }
-        public function getId() { return $this->id; }
+        public function getNomPortfolio() { return $this->nomportfolio; }
+        public function getNomUtilisateur() { return $this->nomutilisateur; }
+        public function getId() { return $this->idportfolio; }
         public function isAccesible() { return $this->accesible; }
     }
 
