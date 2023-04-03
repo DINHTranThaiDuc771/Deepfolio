a07fd808b29ea2148bb3eefe1e9a5d34017d2302
Erreiip <lolpierre154@gmail.com>
2023-04-03 15:18:46 +0200

    Changed utilisateur.inc.php

--- server/Utilisateur.inc.php
+++ server/Utilisateur.inc.php
@@ -2,31 +2,35 @@
 
     class Utilisateur {
 
-        private $nomUtilisateur;
+        private $nomutilisateur;
+        private $mdphash;
+
         private $prenom;
         private $nom;
         private $age;
         private $ville;
         private $universite ;
-        private $mail;
+        private $mailutilisateur;
 
-        public function __construct( $nomUtilisateur = "", $prenom = "", $nom = "", $age = 18, $ville = "", $universite = "", $mail = "" ) {
-            $this->nomUtilisateur = $nomUtilisateur;
+        public function __construct( $nomutilisateur = "", $mdphash = "",$nom = "", $prenom = "", $age = 18, $ville = "", $universite = "", $mailutilisateur = "" ) {
+            $this->nomutilisateur = $nomutilisateur;
+            $this->mdphash = $mdphash;
+
             $this->prenom = $prenom;
             $this->nom = $nom;
             $this->age = $age;
             $this->ville = $ville;
             $this->universite  = $universite ;
-            $this->mail = $mail;
+            $this->mailutilisateur = $mailutilisateur;
         }
 
-        public function getNomUtilisateur() { return $this->nomUtilisateur; }
+        public function getNomUtilisateur() { return $this->nomutilisateur; }
         public function getPrenom() { return $this->prenom; }
         public function getNom() { return $this->nom; }
         public function getAge() { return $this->age; }
         public function getVille() { return $this->ville; }
         public function getUniversite() { return $this->universite; }
-        public function getMail() { return $this->mail; }
+        public function getMail() { return $this->mailutilisateur; }
 
     }
 
