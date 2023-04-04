1cc9f941a0971feb2eb1495f7837a875506b1cb7
BouvetEliott <eli.bvt61@hotmail.com>
2023-04-04 11:38:29 +0200

    Update visualisation.php

--- php/visualisation.php
+++ php/visualisation.php
@@ -43,11 +43,80 @@ function affichePages($username, $idPortfolio, $db){
 
         var_dump($page);
         //$typePage = $page
-
-        //switch($typePage)
+        /*
+        switch($typePage){
+            case 'cv':
+                //TODO: recuperer les infos
+                recupInfosCV($page);
+                break;
+            case 'competences':
+                //TODO: recuperer les infos
+                recupInfosCompetences($page);
+                break;
+            case 'projets':
+                //TODO: recuperer les infos
+                recupInfosProjets($page);
+                break;
+            case 'parcours':
+                //TODO: recuperer les infos
+                recupInfosParcours($page);
+                break;
+            case 'diplomes':
+                //TODO: recuperer les infos
+                recupInfosDiplomes($page);
+                break;
+        }
+        */
     }
 
+}
+
+function recupInfosCV($page){
+    $jsonpage = $page['jsonPage'];
+}
+
+function recupInfosCompetences($page){
+
+    $jsonPage       = $page['jsonPage'];
+    $competences    = $jsonPage->competences; //tableau de competences
+
+    foreach($competences as $competence){
+       new Competence($competence->nom, $competence->description, $competence->lien);
     }
+}
+
+function recupInfosProjets($page){
+
+    $jsonPage       = $page['jsonPage'];
+    $projets        = $jsonPage->projets; //tableau de projets
+
+    foreach($projets as $projet){
+        new Projet($projet->nom, $projet->description, $projet->taille, $projet->lien, $projet->image);
+    }
+
+}
+
+function recupInfosParcours($page){
+
+    $jsonPage       = $page['jsonPage'];
+    $experiences    = $jsonPage->parcours; //tableau d'experiences
+
+    foreach($experiences as $experience){
+        new ExperiencePro($experience->nom, $experience->entreprise, $experience->description, $experience->dateDebut ,$experience->dateFin);
+    }
+
+}
+
+function recupInfosDiplomes($page){
+
+    $jsonPage   = $page['jsonPage'];
+    $diplomes   = $jsonPage->diplomes; //tableau de diplomes
+
+    foreach($diplomes as $diplome){
+        new Diplome($diplome->nom, $diplome->etablissement, $diplome->annee);
+    }
+
+}
 
 ?>
 
