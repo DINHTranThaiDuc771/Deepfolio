e6faa23d6c57eed6263115674861a950227515bf
DINH Tran Thai Duc <ducthai2001177@gmail.com>
2023-04-05 13:19:50 +0200

    Update - Delete comp et projet, Affichage, les structures hmtl

--- js/visualisation.js
+++ js/visualisation.js
@@ -1,16 +1,36 @@
 var isEditing = false;
 var xEditBar;
 var yEditBar;
-var lstEditableText;
+var lstEditableText,lstDeletable,lstButtonSupprimer;
 var editbar;
-window.addEventListener("mousemove", function(event) {
+var btnBold,btnUnderline,btnItalic;
+var selectFont,selectSize;
+btnAjouterProjet.style.display = "none";
+btnAjouterComp  .style.display = "none";
+btnSauver       .style.display = "none";
+
+document.addEventListener("mousemove", function(event) {
     xEditBar = event.clientX;
     yEditBar = event.clientY;
 
     });
 window.onload = () => {
-    editbar = document.getElementById("editbar");
+    editbar         = document.getElementById("editbar");
+    btnBold         = document.getElementById("btnBold");
+    btnUnderline    = document.getElementById("btnUnderline");
+    btnItalic       = document.getElementById("btnItalic");
+    selectFont      = document.getElementById("selectFont");    
+    selectSize      = document.getElementById("selectSize");
 
+    btnBold     .addEventListener("click",()=>{styleBUI("bold")},false);
+    btnUnderline.addEventListener("click",()=>{styleBUI("underline")},false);
+    btnItalic   .addEventListener("click",()=>{styleBUI("italic")},false);
+    selectFont  .addEventListener("change",()=>{
+        styleFont(selectFont.value);
+    });
+    selectSize  ;addEventListener("change",()=>{
+        styleSize(selectSize.value);
+    });
     /**Changer Tab */
     var pageAccueil             = document.getElementById("pageAccueil");
     var pageCompetences         = document.getElementById("pageCompetences");
@@ -31,59 +51,68 @@ window.onload = () => {
     linkContact     .addEventListener("click",function(){changerTab("linkContact");},false);
     
     //Editer
-    lstEditableText = document.getElementsByClassName("editableText");
+    lstEditableText     = document.getElementsByClassName("editableText");
+    lstDeletable        = document.getElementsByClassName("deletetable");
+    lstButtonSupprimer  = document.querySelectorAll('.deletetable button');
 
-    var btnToggleEdit = document.getElementById("btnToggleEdit");
+    var btnToggleEdit   = document.getElementById("btnToggleEdit");
     btnToggleEdit       .addEventListener("click",()=>{toggleEdit()},false);
     //Les btn pour ajouter
     var btnAjouterProjet = document.getElementById("btnAjouterProjet");
     var btnAjouterComp   = document.getElementById("btnAjouterComp");
     btnAjouterProjet.addEventListener("click",ajouterProjet,false);
     btnAjouterComp  .addEventListener("click",ajouterComp  ,false);
+    //
+    var btnHome,btnSauver;
 
+    btnHome = document.getElementById("btnHome");
+    btnSauver = document.getElementById("btnSauver");
+    btnSauver.addEventListener("click",saveEdition,false);
 }
-btnAjouterProjet.style.display = "none";
-btnAjouterComp  .style.display = "none";
-function afficherEditorBar(event){
+
+function afficherEditorBar(){
 
     editbar.style.display="flex";
     editbar.style.left = `${xEditBar}px`;
-    editbar.style.top = `${yEditBar}px`;
+    editbar.style.top = `${yEditBar-50}px`;
 }
 function ajouterProjet()
 {
-    var html = '<div class="row">' +
-    '<div class="mb-5 col-md-4 d-flex justify-content-center">' +
-    '<img src="../img/favicon_io/android-chrome-192x192.png" alt="">' +
-    '</div>' +
-    '<div style="padding:30px;" class="col-md-8 d-flex justify-content-center">' +
-    '<p class="editableText">' +
-    '<strong class="editableText" style="font-size: 24px;">Nom Projet</strong><br>' +
-    '<strong class="editableText">5 personnes</strong><br>' +
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore ' +
-    'et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut ' +
-    'aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse ' +
-    'cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in ' +
-    'culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur ' +
-    'adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim ' +
-    'veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequatunt in ' +
-    'culpa qui officia deserunt mollit anim id est laborum.' +
-    '</p>' +
-    '</div>' +
-    '</div>';
-    btnAjouterProjet.parentNode.insertAdjacentHTML("beforebegin", html);
-    lstEditableText = document.getElementsByClassName("editableText");
-    for (var i=0; i< lstEditableText.length; i++)
+    var html = `
+        <div class="row deletetable">
+        <div class="mb-5 col-md-4 d-flex justify-content-center">
+            <img src="../img/favicon_io/android-chrome-192x192.png" alt="">
+        </div>
+        <div style="padding:30px;" class="col-md-8 d-flex justify-content-center">
 
-    {   
-        lstEditableText[i].setAttribute("contenteditable","true");
-        lstEditableText[i].classList.add("isEditText");
-    }
+            <p style="position: relative;"class="editableText">
+                <strong class="editableText" style="font-size: 24px;">Nom Projet</strong><br>
+                <strong class="editableText">5 personnes</strong>
+                <button><img src="../img/trash.png" alt=""></button> <br>
+
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur
+                adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequatunt in
+                culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+
+        </div>
+
+        </div>    
+    `       
+    ;
+    btnAjouterProjet.parentNode.insertAdjacentHTML("beforebegin", html);
+    refreshListEditable();
+
 }
 function ajouterComp () 
 {
     const newHtml = `
-            <section class="content ">
+            <section class="content deletetable">
             <h1 class="editableText">Elaborer des conceptions simples</h1>
             <article class="editableText">
                 <div class="left">
@@ -92,19 +121,14 @@ function ajouterComp ()
                         <li>Je sais utiliser Github pour gerer les versions de mon projet</li>
                     </ul>
                 </div>
-
             </article>
+            <button ><img src="../img/trash.png" alt=""></button>
+
         </section>
     `;
 
     btnAjouterComp.parentNode.insertAdjacentHTML('beforebegin', newHtml);
-    lstEditableText = document.getElementsByClassName("editableText");
-    for (var i=0; i< lstEditableText.length; i++)
-
-    {   
-        lstEditableText[i].setAttribute("contenteditable","true");
-        lstEditableText[i].classList.add("isEditText");
-    }
+    refreshListEditable();
 }
 function toggleEdit() {
     isEditing = !isEditing;
@@ -116,12 +140,19 @@ function toggleEdit() {
             lstEditableText[i].setAttribute("contenteditable","true");
             lstEditableText[i].setAttribute("tabindex","0");
             lstEditableText[i].addEventListener("focus",afficherEditorBar,false);
-            lstEditableText[i].addEventListener("blur",()=>{editbar.style.display="none";},false);
+
 
             lstEditableText[i].classList.add("isEditText");
         }
+        for (var i=0; i< lstButtonSupprimer.length; i++)
+        {   
+            lstButtonSupprimer[i].addEventListener("click",(event)=>{supprimerDeleteable(event)},false);
+            lstButtonSupprimer[i].style.display = "block";
+
+        }
         btnAjouterProjet.style.display = "inline-block";
         btnAjouterComp  .style.display = "inline-block";
+        btnSauver       .style.display = "inline-block";
         return;
     }
     if (!isEditing)
@@ -133,8 +164,15 @@ function toggleEdit() {
             lstEditableText[i].classList.remove("isEditText");
 
         }
+        for (var i=0; i< lstButtonSupprimer.length; i++)
+        {   
+            lstButtonSupprimer[i].style.display = "none";
+
+        }
         btnAjouterProjet.style.display = "none";
         btnAjouterComp  .style.display = "none";
+        btnSauver       .style.display = "none";
+        editbar         .style.display = "none";
         return;
     }
 }
@@ -192,27 +230,55 @@ function changerTab(tab){
 
 }
 
-winndows.onload = () =>{
-        
-    // Example indexer JavaScript for disabling form submissions if there are invalid fields
-    (() => {
-        'use strict';
 
-        // Fetch all the forms we want to apply custom Bootstrap validation styles to
-        const forms = document.querySelectorAll('.needs-validation');
-        console.log(forms);
+function refreshListEditable() {
+    lstEditableText     = document.getElementsByClassName("editableText");
+    lstDeletable        = document.getElementsByClassName("deletetable");
+    lstButtonSupprimer  = document.querySelectorAll('.deletetable button');
+    //list Editable Text
+    for (var i=0; i< lstEditableText.length; i++)
 
-        // Loop over them and prevent submission
-        Array.prototype.slice.call(forms).forEach((form) => {
-        form.addEventListener('submit', (event) => {
-            
-            if (!form.checkValidity()) {
-            event.preventDefault();
-            event.stopPropagation();
-            }
+    {   
+        lstEditableText[i].setAttribute("contenteditable","true");
+        lstEditableText[i].setAttribute("tabindex","0");
+        lstEditableText[i].classList.add("isEditText");
+        lstEditableText[i].addEventListener("focus",afficherEditorBar,false);
 
-            form.classList.add('was-validated');
-        }, false);
-        });
-    })();
+    }
+    // list button supprimer
+    for (var i=0; i< lstButtonSupprimer.length; i++)
+    {   
+        lstButtonSupprimer[i].addEventListener("click",(event)=>{supprimerDeleteable(event)},false);
+        lstButtonSupprimer[i].style.display = "block";
+    }
+}
+function supprimerDeleteable(event){
+    console.log("supprimer");
+    const deletableDiv = event.target.closest('.deletetable');
+    deletableDiv.style.opacity = '0';
+    setTimeout(() => {
+        deletableDiv.remove();
+    }, 500);
+}
+function styleBUI (style) {
+    const selection = window.getSelection().toString();
+    if (selection.length > 0) {
+        document.execCommand(style);
+    }
+}
+function styleFont(font){
+    const selection = window.getSelection().toString();
+    if (selection.length > 0) {
+        document.execCommand("fontname",false,font);
+    }
+}
+function styleSize(size){
+    const selection = window.getSelection().toString();
+    if (selection.length > 0) {
+        document.execCommand("fontsize",false,size);
+    }
+}
+function saveEdition (){
+    toggleEdit();
+    //Faire ajax pour envoyer le json
 }
