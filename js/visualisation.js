193db4b424fff69c74afa0e8ac3b176855229d2d
nz76 <kzyord@gmail.com>
2023-04-05 16:13:46 +0200

    Add printThis

--- js/visualisation.js
+++ js/visualisation.js
@@ -85,22 +85,7 @@ window.onload = () => {
 
 function telechargerCV()
 {
-    var doc = new jsPDF();
-
-    var specialElementHandlers = {
-        '#editor': function (element, renderer) {
-            return true;
-        }
-    };
-
-    $('#btnTelecharger').click(function() {
-        doc.fromHTML($('#content').html(), 15, 15, {
-            'width': 170,
-            'elementHandlers': specialElementHandlers
-        });
-
-        doc.save('sample-file.pdf');
-    });
+    console.log("dl cv pdf");
 }
 
 function afficherEditorBar(){
