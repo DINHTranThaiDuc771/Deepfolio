c72a4c28ddd0bb680a517f3c328d37a59f132a16
BouvetEliott <eli.bvt61@hotmail.com>
2023-04-05 12:44:10 +0200

    carousel rework

--- php/connexion.php
+++ php/connexion.php
@@ -124,11 +124,101 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     
                   </div>
                 </div>
-                
+                <!--
                 <div id='divImg' class="col-lg-6 d-flex align-items-center slider justify-content-center">  
                   <a type="button" id='previous' style="height: 100%"> &nbsp; </a>
                   <a type="button" id='next' style="height: 100%"> &nbsp; </a>        
-                </div>                
+                </div>  -->
+				<!-- Carousel wrapper -->
+				<div class="col-lg-6 d-flex align-items-center justify-content-center">
+					<div id="carouselBasicExample" class="carousel slide carousel-fade" data-mdb-ride="carousel">
+					<!-- Indicators -->
+					<div class="carousel-indicators">
+						<button
+							type="button"
+							data-mdb-target="#carouselBasicExample"
+							data-mdb-slide-to="0"
+							class="active"
+							aria-current="true"
+							aria-label="Slide 1"
+						></button>
+						<button
+							type="button"
+							data-mdb-target="#carouselBasicExample"
+							data-mdb-slide-to="1"
+							aria-label="Slide 2"
+						></button>
+						<button
+							type="button"
+							data-mdb-target="#carouselBasicExample"
+							data-mdb-slide-to="2"
+							aria-label="Slide 3"
+						></button>
+						<button
+							type="button"
+							data-mdb-target="#carouselBasicExample"
+							data-mdb-slide-to="3"
+							aria-label="Slide 4"
+						></button>
+						<button
+							type="button"
+							data-mdb-target="#carouselBasicExample"
+							data-mdb-slide-to="4"
+							aria-label="Slide 5"
+						></button><button
+							type="button"
+							data-mdb-target="#carouselBasicExample"
+							data-mdb-slide-to="5"
+							aria-label="Slide 6"
+						></button>
+					</div>
+
+					<!-- Inner -->
+					<div class="carousel-inner">
+						<!-- Single item -->
+						<div class="carousel-item active">
+							<img src="../img/image1.jpg" class="d-block w-100" alt="Enzo 1"/>
+						</div>
+
+						<!-- Single item -->
+						<div class="carousel-item">
+							<img src="../img/image2.jpg" class="d-block w-100" alt="Enzo 2"/>
+						</div>
+
+						<!-- Single item -->
+						<div class="carousel-item">
+							<img src="../img/image3.jpg" class="d-block w-100" alt="Duc"/>
+						</div>
+
+						<!-- Single item -->
+						<div class="carousel-item">
+							<img src="../img/image4.jpg" class="d-block w-100" alt="Duc"/>
+						</div>
+
+						<!-- Single item -->
+						<div class="carousel-item">
+							<img src="../img/image5.jpg" class="d-block w-100" alt="Duc"/>
+						</div>
+
+						<!-- Single item -->
+						<div class="carousel-item">
+							<img src="../img/image6.jpg" class="d-block w-100" alt="Duc"/>
+						</div>
+					</div>
+					<!-- Inner -->
+
+					<!-- Controls -->
+					<button class="carousel-control-prev" type="button" data-mdb-target="#carouselBasicExample" data-mdb-slide="prev">
+						<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span class="visually-hidden">Previous</span>
+					</button>
+					<button class="carousel-control-next" type="button" data-mdb-target="#carouselBasicExample" data-mdb-slide="next">
+						<span class="carousel-control-next-icon" aria-hidden="true"></span>
+						<span class="visually-hidden">Next</span>
+					</button>
+					</div>
+					<!-- Carousel wrapper -->        
+				</div>      
               </div>
             </div>
           </div>
@@ -140,3 +230,81 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     <script type="text/javascript"></script>
   </body>
 </html>
+
+
+<?php
+/*
+
+
+    HTML
+
+<!-- Carousel wrapper -->
+<div id="carouselBasicExample" class="carousel slide carousel-fade" data-mdb-ride="carousel">
+  <!-- Indicators -->
+  <div class="carousel-indicators">
+    <button
+      type="button"
+      data-mdb-target="#carouselBasicExample"
+      data-mdb-slide-to="0"
+      class="active"
+      aria-current="true"
+      aria-label="Slide 1"
+    ></button>
+    <button
+      type="button"
+      data-mdb-target="#carouselBasicExample"
+      data-mdb-slide-to="1"
+      aria-label="Slide 2"
+    ></button>
+    <button
+      type="button"
+      data-mdb-target="#carouselBasicExample"
+      data-mdb-slide-to="2"
+      aria-label="Slide 3"
+    ></button>
+  </div>
+
+  <!-- Inner -->
+  <div class="carousel-inner">
+    <!-- Single item -->
+    <div class="carousel-item active">
+      <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(15).webp" class="d-block w-100" alt="Sunset Over the City"/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>First slide label</h5>
+        <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+      </div>
+    </div>
+
+    <!-- Single item -->
+    <div class="carousel-item">
+      <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(22).webp" class="d-block w-100" alt="Canyon at Nigh"/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Second slide label</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+
+    <!-- Single item -->
+    <div class="carousel-item">
+      <img src="https://mdbcdn.b-cdn.net/img/Photos/Slides/img%20(23).webp" class="d-block w-100" alt="Cliff Above a Stormy Sea"/>
+      <div class="carousel-caption d-none d-md-block">
+        <h5>Third slide label</h5>
+        <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+      </div>
+    </div>
+  </div>
+  <!-- Inner -->
+
+  <!-- Controls -->
+  <button class="carousel-control-prev" type="button" data-mdb-target="#carouselBasicExample" data-mdb-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-mdb-target="#carouselBasicExample" data-mdb-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+<!-- Carousel wrapper -->
+*/
+?>
